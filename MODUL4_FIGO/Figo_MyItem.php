2c7515a8d5ee7eb2ceb2b2597e3ca948cc3d6a2c
figosyanan <115631496+figosyanan@users.noreply.github.com>
2022-11-27 21:31:12 +0700

    MODUL4_FIGO

---
+++ MODUL4_FIGO/Figo_MyItem.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  </head>
+
+	<nav class="navbar navbar-expand-lg bg-primary">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav mx-left">
+                    <a class="nav-link text-light" href="Figo_Home.PHP">Home</a>
+                    <a class="nav-link text-light" href="Figo_AddItem.php">MyCar</a>
+                </div>
+                <a href="Figo_AddItem.php" class="btn me-md-2" style="margin-left: 1000px; width: 110px; background-color: white; color:blue; font-weight: 600;">Add Car</a>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        <b><br><h2 style="text-align:left">My Show Room</h2></b>
+        <p class="fw-light" style="text-align:left">List Show Room FIGO - 1202204143</p>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="card" style="width: 30rem;">
+                    <div class="row">
+                        <div class="col">
+                        <img src="hrv.png" class="card-img-top" style="width: 30rem;">
+                          <div class="card-body">
+                            <h5 class="card-title">HRV</h5>
+                            <p class="card-text">Crossover terbaru dari Honda, HRV, hadir dengan 4 varian. Varian tertinggi hadir dengan mesin Petrol 1498 cc...</p>
+                            <a href="#" class="btn btn-primary">Detail</a>
+                            <button type="button" class="btn btn-danger">Delete</button>
+                            </div> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 30rem;">
+                    <div class="row">
+                        <div class="col">
+                        <img src="pajero.png" class="card-img-top"  height="270px" width="30px">
+                          <div class="card-body">
+                            <h5 class="card-title">Pajero Sport</h5>
+                            <p class="card-text">Pajero Sport generasi pertama dikenal dengan nama Mitsubishi Challenger yang diluncurkan pada 1996 silam...</p>
+                            <a href="#" class="btn btn-primary">Detail</a>
+                            <button type="button" class="btn btn-danger">Delete</button> 
+                        </div>
+                        </div>           
+                    </div>
+                </div>
+            </div>
+
+    <div class="container">
+            <div class="col">
+                <div class="card" style="width: 30rem;">
+                    <div class="row">
+                        <div class="col">
+                            <img src="fortuner.png" class="card-img-top" alt="fortuner" />
+                            <div class="card-body">
+                                <h5 class="card-title">Fortuner</h5>
+                                <p class="card-text">Toyota Fortuner tersedia dalam pilihan mesin 10 Diesel dan 2 Petrol di Indonesia SUV baru dari Toyota hadir...</p>
+                                <a href="#" class="btn btn-primary">Detail</a>
+                                <button type="button" class="btn btn-danger">Delete</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+
+    <div class="col">
+    <br><br><p>Jumlah Mobil: 3</p>
+    </div>
---
+++ MODUL4_FIGO/wad_modul4_figo.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 27, 2022 at 03:24 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `wad_modul4_figo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_figo`
+--
+
+CREATE TABLE `user_figo` (
+  `id` bigint(20) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `no_hp` varchar(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_figo`
+--
+
+INSERT INTO `user_figo` (`id`, `nama`, `email`, `password`, `no_hp`) VALUES
+(1, 'sfsa', 'fsafas', 'sdfsafsdasdsa', 'safsa'),
+(2, 'fsafasfa', 'sfafa', 'fsafas', 'fsafsaf'),
+(7, 'fsafasf', 'fsafa', 'fsafasf', 'fsafasf'),
+(8, 'Figo', 'figosyanan@student.telkomuniversity.ac.id', 'sadasdasdas', '08122142142'),
+(9, 'fsafas', 'safas', 'dsadas', 'dsadasd');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user_figo`
+--
+ALTER TABLE `user_figo`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user_figo`
+--
+ALTER TABLE `user_figo`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
